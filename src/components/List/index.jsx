c627a2e7d538236e2a4d2c4115f5409d2a491ace
joão souza <jvictorcs35@gmail.com>
2026-04-16 19:47:34 -0300

    oi

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,26 +1,25 @@
 import './style.css'
 
-function List({
-  title,
-  subtitleSecondList = "Minha Lista de Compras"
-}) {
-  <section>
-    <h2 id='exemplo-h2'>{title}</h2>
+function List({ title, subtitleSecondList = "Minha Lista de Compras" }) {
+  return (
+    <section>
+      <h2 id='exemplo-h2'>{title}</h2>
 
-    <h3>{subtitleSecondList}</h3>
-    <ul id='ul'>
-      <li>batata</li>
-      <li>sabor</li>
-      <li>pão</li>
-    </ul>
+      <h3>{subtitleSecondList}</h3>
+      <ul id='ul'>
+        <li>batata</li>
+        <li>sabor</li>
+        <li>pão</li>
+      </ul>
 
-    <h3>Lista Ordenada</h3>
-    <ol>
-      <li>Scooby-Doo</li>
-      <li>Pica-Pau</li>
-      <li>Professor Renan</li>
-    </ol>
-  </section>
+      <h3>Lista Ordenada</h3>
+      <ol>
+        <li>Scooby-Doo</li>
+        <li>Pica-Pau</li>
+        <li>Professor Renan</li>
+      </ol>
+    </section>
+  )
 }
 
 export default List
